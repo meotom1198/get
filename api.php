8e86e29cf32f0e1dce91c891049e34dc3d89dd3d
meotom1198 <81725891+meotom1198@users.noreply.github.com>
2025-03-20 04:21:20 +0700

    Create api.php

---
+++ api.php
@@ -0,0 +1,44 @@
+{
+    "success": true,
+    "title": "8.0.2",
+    "content": "<center><img src=\"https://wapvn.top/images/logo.png\" alt=\"WapVN.TOP\" title=\"WapVN.TOP\"></center>
+<center><h1 style=\"color:blue;\">Joe+ x Typecho VN</h1></center>
+<center><b style=\"color:violet;\">WapVN.TOP - Blog Cá Nhân Của DJ MeoDiLac</b></center>
+<br/>
+<span>
+    <h2 style=\"color:rgb(255, 0, 0);\">Giới thiệu</h2>
+        <ul>
+            <li><b style=\"color:violet;\">WapVN.TOP</b> là nơi mình chia sẻ các bài viết về nhiều chủ đề khác nhau như: chia sẻ các loại code, kiến thức lập trình, âm nhạc, meme, sound effect, font chữ, v.v..</li>
+            <li>Và mình cũng nhận hỡ trợ tạo blog miễn phí, với điều kiện bạn phải đang sử dụng mã nguồn <a href=\"https://wapvn.top/typecho-121-special-mod-theme-joe-799-special-mod.html\"><b style=\"color:rgb(255, 153, 0);\">Typecho VN</b></a> được download tại <a href=\"https://wapvn.top/\"><b style=\"color:rgb(0, 4, 255);\">WapVN.TOP</b></a></li>
+            <li>Hãy ủng hô bằng cách chuyển tiền vào: <b>VIETCOMBANK</b> - <b>9789423706</b> - <b>PHAM DINH LUYEN</b> Xin cảm ơn.</li>
+        </ul>
+</span>
+<br/>
+<span>
+    <h3 style=\"color:rgb(0, 142, 167);\">Thông tin</h3>
+        <ul>
+            <li><b>Facebook:</b> <a href=\"https://facebook.com/cu.ti.9212\">Luyện Phạm</a></li>
+            <li><b>Trang chủ:</b> <a href=\"https://wapvn.top\">WapVN.TOP</a></li>
+            <li><b>Upload:</b> <a href=\"https://upload.wapvn.top\">Upload.WapVN.TOP</a></li>
+        </ul>
+</span>
+<br/>
+<span>
+    <h3 style=\"color:rgb(36, 0, 167);\">Chi tiết cập nhật</h3>
+        <ol>
+            <li><b>Thông báo</b> - Theme Joe+ 8.0.2 đã có phần thông báo riêng. <i>Cài đặt theme</i> => <i>Thông báo</i>.</li>
+            <li><b>Fix lỗi, sau khi edit bài viết hoặc trang thì xảy ra hiện tượng bài viết hoặc trang vừa sửa bị ẩn đi.</b></li>
+            <li><b>Cài đặt theme</b> - Lược bớt các tính năng không dùng được và sửa lại đổi chút để đỡ nhàm chán.</li>
+            <li><b>Quảng cáo</b> - Hiện tại có các tuỳ chỉnh <i>Quảng táo</i> như sau:</li>
+                    <ol>
+                        <li><b>ADS thanh nav</b> - Bên dưới thông báo (PC và Mobile thấy).</li>
+                        <li><b>ADS trang chủ</b> - Bên trên bài viết mới, bên dưới bài viết trượt (PC và Mobile thấy).</li>
+                        <li><b>ADS trang chủ</b> - Bên trên bài viết mới, bên dưới bài viết trượt (PC và Mobile thấy).</li>
+                        <li><b>ADS trong bài viết</b> -  Bên dưới thông báo ghi nguồn khi leech bài </li>
+                        <li><b>Đang upate thêm..</b> - Cùng đón chờ nha!</li>
+                    </ol>
+            <li><b>Đang upate thêm..</b> - Cùng đón chờ nha!</li>
+        </ol>
+    </span>",
+    "update": "2025-03-10 08:00:00"
+}
